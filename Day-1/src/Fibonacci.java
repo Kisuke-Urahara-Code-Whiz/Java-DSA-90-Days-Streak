94fdd0c9a3b3a396b5f10195416261c0074c5ed5
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-04-19 01:19:59 +0530

    completed three porgrams

--- Day-1/src/Fibonacci.java
+++ Day-1/src/Fibonacci.java
@@ -5,6 +5,14 @@ public class Fibonacci {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of terms in the Fibonacci series: ");
         int n = sc.nextInt();
-        
+        int a = 0, b = 1;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        for(int i=1;i<=(n-2);i++){
+            int t = b;
+            a = a+b;
+            b = t;
+            System.out.print(a+" ");
+        }
     }
 }
