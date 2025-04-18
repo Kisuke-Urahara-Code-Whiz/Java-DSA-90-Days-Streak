41501788357621ab948a5fad8cdda44ba0ef1b40
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-04-19 00:52:55 +0530

    Commits

---
+++ Day-1/src/Fibonacci.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms in the Fibonacci series: ");
+        int n = sc.nextInt();
+        
+    }
+}
