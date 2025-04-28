f7fc0d1235eeb68bfd51536fcb69ef0777b07620
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-04-28 20:20:25 +0530

    Create Switch.java

---
+++ Day-2/src/Switch.java
@@ -0,0 +1,10 @@
+public class Switch {
+    public static void main(String[] args) {
+        int a = 5;
+        switch(a){
+            case 1 -> System.out.println("1");
+            case 2 -> System.out.println("2");
+            default -> System.out.println("Default");
+        }
+    }
+}
