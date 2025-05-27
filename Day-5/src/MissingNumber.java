de55334bb5cd80a826b9127d11e07e1c93f79754
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-05-28 00:10:33 +0530

    Create MissingNumber.java

---
+++ Day-5/src/MissingNumber.java
@@ -0,0 +1,92 @@
+//LEETCODE EASY
+//268. Missing Number
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [3,0,1]
+//
+//Output: 2
+//
+//Explanation:
+//
+//n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+//
+//Example 2:
+//
+//Input: nums = [0,1]
+//
+//Output: 2
+//
+//Explanation:
+//
+//n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+//
+//Example 3:
+//
+//Input: nums = [9,6,4,2,3,5,7,0,1]
+//
+//Output: 8
+//
+//Explanation:
+//
+//n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//Constraints:
+//
+//n == nums.length
+//1 <= n <= 104
+//        0 <= nums[i] <= n
+//All the numbers of nums are unique.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of integers in the array : ");
+        int n =  sc.nextInt();
+        int[] arr  = new int[n];
+        for(int i=0;i<n;i++) {
+            System.out.println("Enter value for index "+i+" : ");
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Original array : ");
+        display(arr);
+        if(checkCorrect(arr)){
+            findDisappeared(arr);
+        }
+        else{
+            System.out.println("Invalid numbers entered in the array. Array must contain only [1,n] numbers");
+        }
+    }
+
+    public static void display(int[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+    public static boolean checkCorrect(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>arr.length)
+                return false;
+        }
+        return true;
+    }
+}
