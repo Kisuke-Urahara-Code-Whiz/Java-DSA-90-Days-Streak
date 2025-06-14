6655beff119e66102059230f1fa326070f24c2f3
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-06-15 01:31:44 +0530

    started with linked list for the first time

--- Day-8/src/Singly_LL.java
+++ Day-8/src/Singly_LL.java
@@ -1,2 +1,60 @@
 public class Singly_LL {
+
+    private Node head;
+    private int size;
+
+    public Singly_LL(){
+        this.size = 0;
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+        size+=1;
+    }
+
+    public void insertLast(int value){
+        if(size==0)
+            insertFirst(value);
+        else{
+            Node node = new Node(value);
+            node.next = null;
+            Node temp = head;
+            int a = 1;
+            while(a<=size-1){
+                temp=temp.next;
+                a++;
+            }
+            temp.next = node;
+        }
+
+    }
+
+    public void display() {
+        Node temp = head;
+        System.out.println("Linked List:");
+        System.out.print("START -> ");
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+
 }
--- Day-8/src/Singly_LL_Ops.java
+++ Day-8/src/Singly_LL_Ops.java
@@ -1,2 +1,10 @@
 public class Singly_LL_Ops {
+    public static void main(String[] args) {
+        Singly_LL list = new Singly_LL();
+        list.insertLast(1);
+        list.insertFirst(2);
+        list.insertLast(3);
+        list.insertLast(3);
+        list.display();
+    }
 }
