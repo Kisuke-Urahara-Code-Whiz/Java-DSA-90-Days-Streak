f3fb66da09d0482c61699a704a586c68cb410658
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-09-24 14:53:17 +0530

    Update LeetCode2_More_Optimized.java

--- LeetCodeMedium/LeetCode2_More_Optimized.java
+++ LeetCodeMedium/LeetCode2_More_Optimized.java
@@ -32,4 +32,57 @@ public class LeetCode2_More_Optimized {
 
         return dummy.next;
     }
+
+    public static void main(String[] args) {
+        LeetCode2_More_Optimized solution = new LeetCode2_More_Optimized();
+
+        int[] arr1 = {5};
+        int[] arr2 = {5};
+
+        ListNode l1 = solution.createLinkedList(arr1);
+        ListNode l2 = solution.createLinkedList(arr2);
+
+        System.out.print("Input List 1: ");
+        printLinkedList(l1);
+
+        System.out.print("Input List 2: ");
+        printLinkedList(l2);
+
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        System.out.print("Result List:  ");
+        printLinkedList(result);
+    }
+
+    public ListNode createLinkedList(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        ListNode head = null;
+        ListNode current = null;
+        for(int i=0; i<arr.length; i++){
+            if(head==null){
+                ListNode temp = new ListNode(arr[i]);
+                head = temp;
+                current = temp;
+            }
+            else {
+                ListNode temp = new ListNode(arr[i]);
+                current.next = temp;
+                current = temp;
+            }
+        }
+
+        return head;
+    }
+
+    public static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 }
