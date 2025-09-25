9643f30df12b1560860daa14449eb9d49e872de5
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-09-25 20:50:41 +0530

    Create LeetCode17.java

---
+++ LeetCodeMedium/LeetCode17.java
@@ -0,0 +1,24 @@
+package LeetCodeMedium;
+
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode17 {
+
+//    Map<Character, String[]> employeeMap = Map.of(
+//            '2', "abc",
+//            '3', "def",
+//            '4', "ghi",
+//            '5', "jkl",
+//            '6', "mno",
+//            '7', "pqrs",
+//            '8', "tuv",
+//            '9', "wxyz"
+//    );
+//
+//    public List<String> letterCombinations(String digits) {
+//        List<String> res = new List<>()
+//    }
+
+
+}
