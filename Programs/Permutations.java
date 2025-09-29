cf5070507bc673cdfc034a315514a591db6079c2
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-09-29 17:27:33 +0530

    wrote permutations
    took me like seconds lmfao

--- Programs/Permutations.java
+++ Programs/Permutations.java
@@ -1,4 +1,22 @@
 package Programs;
 
 public class Permutations {
+
+    public void permutations(String processed, String unprocessed){
+        if(unprocessed.isEmpty())
+            System.out.println(processed);
+        else{
+            for(int i=0;i<unprocessed.length();i++){
+                permutations(processed+unprocessed.charAt(i), unprocessed.substring(0,i)+unprocessed.substring(i+1));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        Permutations obj = new Permutations();
+        System.out.println("Original String -> "+s);;
+        System.out.println("Permutations -> ");
+        obj.permutations("", s);
+    }
 }
