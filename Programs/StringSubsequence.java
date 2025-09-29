0779a11b9313c530d8f0155da0d116803132b413
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-09-29 12:20:47 +0530

    learning subsequences ops

--- Programs/StringSubsequence.java
+++ Programs/StringSubsequence.java
@@ -1,6 +1,19 @@
 package Programs;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringSubsequence {
+
+    Set<String> strings;
+    ArrayList<String> stringList;
+
+
+    StringSubsequence(){
+        strings = new HashSet<>();
+        stringList = new ArrayList<>();
+    }
 
     public void subsequence(String s, String str){
         if(str.length()!=0){
@@ -14,21 +27,38 @@ public class StringSubsequence {
             }
         }
         else{
-            if(s.isEmpty()){
-                System.out.println("NULL");
+            stringList.add(s);
+        }
+    }
+
+    public void repeatingSubsequence(String s, String str){
+        if(str.length()!=0){
+            if(str.length()!=1) {
+                repeatingSubsequence(s + str.charAt(0), str.substring(1,str.length()));
+                repeatingSubsequence(s, str.substring(1,str.length()));
             }
             else{
-                System.out.println(s);
+                repeatingSubsequence(s+str.charAt(0), "");
+                repeatingSubsequence(s, "");
+            }
+        }
+        else{
+            if(!strings.contains(s)){
+                strings.add(s);
             }
         }
     }
 
     public static void main(String[] args) {
-        String a = "abcde";
+        String a = "aab";
         System.out.println("Original String -> "+a);
         StringSubsequence obj = new StringSubsequence();
         System.out.println("Subsequences -> ");
         obj.subsequence("",a);
+        System.out.println(obj.stringList);
+        System.out.println("Non-Repeating Subsequences -> ");
+        obj.repeatingSubsequence("",a);
+        System.out.println(obj.strings);
     }
 
 }
