f8839118fb952d56bfcf20c3d0f53351f36ad474
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-10-10 02:05:22 +0530

    done

--- LeetCodeMedium/LeetCode38.java
+++ LeetCodeMedium/LeetCode38.java
@@ -39,30 +39,3 @@ public class LeetCode38 {
 
 }
 
-//public String countAndSay(int n) {
-//    String res = "1";
-//    for (int i = 1; i < n; i++) {
-//        res = buildNext(res);
-//    }
-//    return res;
-//}
-//
-//private String buildNext(String s) {
-//    StringBuilder sb = new StringBuilder(s.length() * 2); // pre-size
-//    char prev = s.charAt(0);
-//    int count = 1;
-//
-//    for (int i = 1; i < s.length(); i++) {
-//        char c = s.charAt(i);
-//        if (c == prev) {
-//            count++;
-//        } else {
-//            sb.append(count).append(prev);
-//            prev = c;
-//            count = 1;
-//        }
-//    }
-//    sb.append(count).append(prev);
-//    return sb.toString();
-//}
-
